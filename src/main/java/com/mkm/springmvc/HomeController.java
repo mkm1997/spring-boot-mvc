3658671874153ab0ede47a9c09dc54d2b599b39d
Manish Mishra <manismishra@paypal.com>
2021-08-26 15:30:19 +0530

    request params .

--- src/main/java/com/mkm/springmvc/HomeController.java
+++ src/main/java/com/mkm/springmvc/HomeController.java
@@ -1,6 +1,7 @@
 package com.mkm.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -17,14 +18,24 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(HttpServletRequest req){
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
-        System.out.println("First number is: " + num1 + "Second number is: " + num2);
+    public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, HttpSession session){
 
-        int result = num1 + num2;
-        HttpSession session = req.getSession();
+        System.out.println("First number is: " + i + " Second number is: " + j);
+        int result = i + j;
         session.setAttribute("num", result);
         return "result.jsp";
     }
+
+
+//    @RequestMapping("/add")
+//    public String add(HttpServletRequest req){
+//        int num1 = Integer.parseInt(req.getParameter("num1"));
+//        int num2 = Integer.parseInt(req.getParameter("num2"));
+//        System.out.println("First number is: " + num1 + "Second number is: " + num2);
+//
+//        int result = num1 + num2;
+//        HttpSession session = req.getSession();
+//        session.setAttribute("num", result);
+//        return "result.jsp";
+//    }
 }
